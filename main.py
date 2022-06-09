7bfcd147d5fd173739b1c91254dbb16b3c953980
mrtedn21 <bezgin.sasha06@gmail.com>
2022-06-09 23:04:55 +0700

    using context manager as screen

--- main.py
+++ main.py
@@ -3,44 +3,44 @@ import time
 from datetime import datetime
 
 
+class Screen:
+    def __enter__(self):
+        self.screen = curses.initscr()
+        self.screen.nodelay(True)
+        curses.noecho()
+        curses.cbreak()
+        self.screen.keypad(True)
+        return self.screen
+
+    def __exit__(self, *exceptions):
+        curses.nocbreak()
+        self.screen.keypad(False)
+        curses.echo()
+        curses.endwin()
+
 def main():
-    screen = curses.initscr()
-    screen.nodelay(True)
-    curses.noecho()
-    curses.cbreak()
-    screen.keypad(True)
-    res = []
-    initial_time = datetime.now()
-    x = 1
-    y = 1
+    with Screen() as screen:
+        res = []
+        initial_time = datetime.now()
+        x = 1
+        y = 1
 
-    while True:
         key = screen.getch()
-        if key > 0:
-            res.append(key)
-        if key == 259:
-            x += 1
-            y -= 1
-        if key == 258:
-            x += 1
-            y += 1
-        screen.addstr(y, x, 'x')
-        if (datetime.now() - initial_time).total_seconds() > 10:
-            break
+        while key != 27:
+            key = screen.getch()
+            if key > 0:
+                res.append(key)
+            if key == 259:
+                x += 1
+                y -= 1
+            if key == 258:
+                x += 1
+                y += 1
+            screen.addstr(y, x, 'x')
 
-    curses.nocbreak()
-    screen.keypad(False)
-    curses.echo()
-    curses.endwin()
-    print(res)
+        print(res)
 
 
 if __name__ == '__main__':
     main()
-    #try:
-    #    main()
-    #except BaseException as e:
-    #    raise e
-    #finally:
-    #    curses.endwin()
 
