446f68216537a8292649537081212e20d4534640
alexander.bezgin <alexander.bezgin@balance-pl.ru>
2024-01-24 13:19:08 +0700

    change border printing for more beautiful

--- terminal.py
+++ terminal.py
@@ -5,7 +5,6 @@ from gameplay_utils import Cell
 
 cell_type_to_terminal_char = {
     Cell.EMPTY: ' ',
-    Cell.BORDER: '█',
     Cell.TRACK: 'X',
     Cell.CONSIDER: ' ',
     Cell.MARKED: 'X',
@@ -20,6 +19,8 @@ class Terminal:
         curses.noecho()
         curses.cbreak()
         self._screen_obj.keypad(True)
+
+        self.max_y, self.max_x = self.get_max_y_and_x()
 
     def get_max_y_and_x(self):
         return self._screen_obj.getmaxyx()
@@ -38,10 +39,35 @@ class Terminal:
 
     def print_changes(self, changes: list):
         for y, x, cell_type in changes:
-            char = cell_type_to_terminal_char[cell_type]
-            try:
-                self._screen_obj.addch(y, x, char)
-            except curses.error:
-                pass
+            if cell_type == Cell.BORDER:
+                self._print_border(y, x)
+            else:
+                char = cell_type_to_terminal_char[cell_type]
+                self._print(y, x, char)
 
         self._screen_obj.refresh()
+
+    def _print(self, y, x, char):
+        try:
+            self._screen_obj.addch(y, x, char)
+        except curses.error:
+            pass
+
+    def _print_border(self, y: int, x: int):
+        if y == self.max_y - 1 and x == self.max_x - 1:
+            self._print(y, x, '┘')
+
+        elif y == 0 and x == 0:
+            self._print(y, x, '┌')
+
+        elif y == self.max_y - 1 and x == 0:
+            self._print(y, x, '└')
+
+        elif y == 0 and x == self.max_x - 1:
+            self._print(y, x, '┐')
+
+        elif y == 0 or y == self.max_y - 1:
+            self._print(y, x, '─')
+
+        elif x == 0 or x == self.max_x - 1:
+            self._print(y, x, '│')
